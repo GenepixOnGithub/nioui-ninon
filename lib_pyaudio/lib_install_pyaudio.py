5abe057f52af7085f137f6ba2bef9fee7876808c
PAVfr <paul145andre@live.fr>
2021-02-01 10:09:48 +0100

    Update lib_install_pyaudio.py

--- lib_pyaudio/lib_install_pyaudio.py
+++ lib_pyaudio/lib_install_pyaudio.py
@@ -13,12 +13,12 @@ def install():
     # Python Version
     version = sys.version.split(" ", maxsplit=1)[0].split(".")
     # Path WHL
-    m = "" if int("".join(version[:2])) >= 38 else "m"
+    m = "" if int("".join(version[:2])) >= 37 else "m"
     name_file = "PyAudio-0.2.11-cp{version}-cp{version}{m}-{architecture}.whl".format(version="".join(version[:2]), architecture=architecture, m=m)
     path = os.path.join("lib_pyaudio", name_file)
     print(path)
     if not os.path.exists(path):
-        raise Exception("Check your version : Python 3.4 >= 3.9")
+        raise Exception("Check your version : Python 3.6 >= 3.9")
     # Install Package
     subprocess.check_call([sys.executable, "-m", "pip", "install", path])
 
